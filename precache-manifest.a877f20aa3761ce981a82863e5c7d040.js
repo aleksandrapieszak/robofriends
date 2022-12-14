cb287982aba15aa3cb55846784282aa5d7897a58
Aleksandra Pieszak <aleksandraapieszak@gmail.com>
2022-12-14 11:40:38 +0100

    Updates

---
+++ precache-manifest.a877f20aa3761ce981a82863e5c7d040.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b345ca5637717d8fec19",
+    "url": "/robofriends/static/css/main.64df4105.chunk.css"
+  },
+  {
+    "revision": "b345ca5637717d8fec19",
+    "url": "/robofriends/static/js/main.b345ca56.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriends/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "230a0921d75b76bef5df",
+    "url": "/robofriends/static/css/2.29ab65bb.chunk.css"
+  },
+  {
+    "revision": "230a0921d75b76bef5df",
+    "url": "/robofriends/static/js/2.230a0921.chunk.js"
+  },
+  {
+    "revision": "6bf665704170461feaf8",
+    "url": "/robofriends/static/js/runtime~main.6bf66570.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "541cf99bd6c047cddbd18f64ada8f76d",
+    "url": "/robofriends/index.html"
+  }
+];
